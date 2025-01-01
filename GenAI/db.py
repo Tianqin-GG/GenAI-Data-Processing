88729f9131f214b3cb03c181e7ca332c0cccf5b8
Tianqin-GG <tianqin.zhang@galengrowth.com>
2025-01-01 20:11:46 +0800

    cleaned dir

--- GenAI/db.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-data = pd.read_csv('to_add.csv')
-print(data.head())
--- GenAI/get_corporate_cate.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-cate = pd.read_csv('./data/part.csv')
-# print(category.head())
-category = []
-for i in cate['name']:
-    if i not in category:
-        category.append(i)
-print(category)
